75018d99cb864f5658dc4dfba96db1a0e7d8db2a
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-04-29 15:50:39 +0530

    non-static method access by reference of class

--- eclipse-workspace/multidimentionalarrays/src/main/java/com/ibm/multidimentionalarrays/Application.java
+++ eclipse-workspace/multidimentionalarrays/src/main/java/com/ibm/multidimentionalarrays/Application.java
@@ -1,36 +1,18 @@
 package com.ibm.multidimentionalarrays;
-//class Point{
-//	public int x;
-//	public int y;
-//}
-
+ 
 class Data{
 	static public int value;
+	public int getValue(){
+		return value;
+	}
 }
 public class Application
 {
+	int count = 34;
 	public static void main( String[] args )
 	{
-//		int nums[][];
-//		nums = new int[2][5];
-//		for(int[]row:nums) {
-//			for(int values:row) {
-//				System.out.print(values);
-//			}
-//			System.out.println();
-//		}
-		/*
-		Point points[];
-		points = new Point[3];
-		points[0] = new Point();
-		points[0].x=3;
-		*/
-		Data data1 = new Data();
-		Data data2 = new Data();
-		data1.value = 3;
-		data2.value = 5;
-		Data.value = 44;
-		System.out.println(data1.value+"\t"+data2.value);
+		Application application = new Application();
+		System.out.println(application.count);
 		
 		
 	}
