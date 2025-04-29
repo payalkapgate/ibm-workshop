28eb2df081d56b12d7f16ee3df2657d1914b6da9
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-04-29 14:29:43 +0530

    reference type  arrays

--- eclipse-workspace/multidimentionalarrays/src/main/java/com/ibm/multidimentionalarrays/Application.java
+++ eclipse-workspace/multidimentionalarrays/src/main/java/com/ibm/multidimentionalarrays/Application.java
@@ -1,17 +1,25 @@
 package com.ibm.multidimentionalarrays;
-
+class Point{
+	public int x;
+	public int y;
+}
 public class Application
 {
 	public static void main( String[] args )
 	{
-		int nums[][];
-		nums = new int[2][5];
-		for(int[]row:nums) {
-			for(int values:row) {
-				System.out.print(values);
-			}
-			System.out.println();
-		}
+//		int nums[][];
+//		nums = new int[2][5];
+//		for(int[]row:nums) {
+//			for(int values:row) {
+//				System.out.print(values);
+//			}
+//			System.out.println();
+//		}
+		Point points[];
+		points = new Point[3];
+		points[0] = new Point();
+		points[0].x=3;
+		
 		
 	}
 }
