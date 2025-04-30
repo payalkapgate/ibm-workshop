50cebe3c4d71aa684994fe781e1d68ca12f99ef3
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-04-30 15:58:51 +0530

    interface

--- eclipse-workspace/oops/src/main/java/com/ibm/oops/Application.java
+++ eclipse-workspace/oops/src/main/java/com/ibm/oops/Application.java
@@ -1,8 +1,24 @@
 package com.ibm.oops;
+interface Printer{
+	void print();
+}
+class HPPrinter implements Printer{
+	@Override
+	public void print() {
+		System.out.println("HP");
+	}
+}
+class Process{
+	public void execute(Printer printer) {
+		printer.print();
+	}
+}
 
 public class Application
 {
 	public static void main( String[] args )
 	{
+		Process process = new Process();
+		process.execute(new HPPrinter());
 	}
 }
--- eclipse-workspace/oops/src/main/java/com/ibm/oops/pack2/Deived1.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.oops.pack2;
-import com.ibm.oops.base1.*;
-
-public class Deived1 extends Base1 {
-	public void accessPubf() {
-		pubf=19;
-		prof=23;
-	}
-}
