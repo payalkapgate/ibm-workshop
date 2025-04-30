c81ee5cb64f460bdd590c68bde3b9909d2624e76
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-04-30 10:11:28 +0530

    enum add the method and its values also by using initiazing varialble how to access contructor by using userdefine method

--- eclipse-workspace/enums/src/main/java/com/ibm/enums/Application.java
+++ eclipse-workspace/enums/src/main/java/com/ibm/enums/Application.java
@@ -1,20 +1,27 @@
 package com.ibm.enums;
 
  enum Apple{
-		GoldenDel,
-		RedDel,
-		Dala;
+		GoldenDel(10),
+		RedDel(20),
+		Dala(30);
+	 private int price;
+	 Apple(int cost){
+		 price=cost;
+	 }
+	 public int getPrice(){
+		 return price;
+	 }
 	}
 
 public class Application
 {
 	public static void main( String[] args )
 	{
-		Apple applee = Apple.RedDel;
-		System.out.println(applee);
+//		Apple applee = Apple.RedDel;
+//		System.out.println(applee);
 		Apple[] apples = Apple.values();
 		for(Apple apple:apples) {
-			System.out.println(apple);
+			System.out.println(apple+" costs: "+apple.getPrice());
 		}
 		
 		
