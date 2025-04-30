fc57976a75570b389945dad810f3d2fddbf12521
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-04-30 11:27:06 +0530

    Enecapsulation

---
+++ eclipse-workspace/oops/src/main/java/com/ibm/oops/base1/Base1.java
@@ -0,0 +1,12 @@
+package com.ibm.oops.base1;
+
+public class Base1 {
+	public int pubf;
+	private int prif;
+	protected int prof;
+	int diff;
+	 public void accessPubf() {
+		 pubf = 5;
+	 }
+
+}
---
+++ eclipse-workspace/oops/src/main/java/com/ibm/oops/base1/Base2.java
@@ -0,0 +1,10 @@
+package com.ibm.oops.base1;
+
+public class Base2 {
+	public void accessPubf() {
+//		int pubf = 13;
+		Base1 base1 = new Base1();
+		base1.pubf=12;
+	}
+	
+}
