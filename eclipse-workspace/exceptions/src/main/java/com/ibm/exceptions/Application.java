d133202b3fc0249c7347a741f37f643f7bf09e81
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-05-02 10:34:27 +0530

    Using Or And default exceptions

--- eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Application.java
+++ eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Application.java
@@ -6,9 +6,15 @@ public class Application
 	{
 		System.out.println("Start");
 		try {
-			System.out.println(args[0]);
-		}catch(ArrayIndexOutOfBoundsException ex) {
-			System.out.println("Run program supplying a number");
+			System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));
+		}catch(ArrayIndexOutOfBoundsException  | NumberFormatException exception) {
+			System.out.println("Make sure you provide atleast 2 correct right argument");
+		}
+		catch(ArithmeticException ex) {
+			System.out.println("You cannot divided by zero ");
+		}
+		catch(Exception exception) {
+			System.out.println("Unkonown Issue");
 		}
 		System.out.println("End");
 	}
