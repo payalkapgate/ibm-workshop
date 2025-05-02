1fcb2fb6650447c59702bf2120eac083b3553531
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-05-02 16:04:27 +0530

    use lambda function and use make one method and put in front of arrow

--- eclipse-workspace/threads/src/main/java/com/ibm/threads/Application.java
+++ eclipse-workspace/threads/src/main/java/com/ibm/threads/Application.java
@@ -1,18 +1,16 @@
 package com.ibm.threads;
-class Runner implements Runnable{
-	@Override
-	public void run() {
+ 
+public class Application
+{
+	public static void workLoad() {
 		for(int c=0;c<10;c++) {
 			System.out.println(Thread.currentThread()+"--->"+String.valueOf(c));
 		}
 	}
-}
-public class Application
-{
 	public static void main( String[] args )
 	{
-		Runner runner = new Runner();
-		Thread thread1 = new Thread(runner);
+		
+		Thread thread1 = new Thread(() -> workLoad());
 		System.out.println(thread1.getState());
 		thread1.start();
 		System.out.println(thread1.getState());
