0d8e813ec1c9e8e0ce697fa26af119c6f7eff38f
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-05-02 12:34:48 +0530

    Assertion

--- eclipse-workspace/throwexception/src/main/java/com/ibm/throwexception/Application.java
+++ eclipse-workspace/throwexception/src/main/java/com/ibm/throwexception/Application.java
@@ -1,31 +1,15 @@
 package com.ibm.throwexception;
-class IllegalAgeException extends Throwable{
-	@Override
-	public String getMessage() {
-		// TODO Auto-generated method stub
-		return  "You are not fit for voting";
-	}
-}
-class ElectionVoting{
-	public void register(int age) throws IllegalAgeException{
-		if(age<18) {
-			throw new IllegalAgeException();
-		}
-		System.out.println("Registration completed");
-	}
-}
+
+import com.ibm.throwexception.math.Arithmetic;
+
+
 public class Application
 {
 	public static void main(String[] args)
 	{
 		System.out.println("start");
-		ElectionVoting electionVoting = new ElectionVoting();
-		try {
-			electionVoting.register(17);
-		} catch (IllegalAgeException e) {
-			 
-			e.printStackTrace();
-		}
+		Arithmetic arithmetic = new Arithmetic();
+		assert (arithmetic.sum(2,4)==6):"The sum method is defective";
 		System.out.println("End");
 	}
 }
---
+++ eclipse-workspace/throwexception/src/main/java/com/ibm/throwexception/math/Arithmetic.java
@@ -0,0 +1,9 @@
+package com.ibm.throwexception.math;
+
+public class Arithmetic {
+	int iNum1, iNum2;
+	public int sum(int iNum1, int iNum2) {
+		return iNum1*iNum2;
+	}
+
+}
