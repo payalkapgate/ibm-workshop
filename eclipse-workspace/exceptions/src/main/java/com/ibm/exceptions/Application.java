1f2e47ce8bded3a403926c3849ebef6efe383c35
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-05-02 11:24:37 +0530

    finally and nested try catch

--- eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Application.java
+++ eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Application.java
@@ -5,8 +5,22 @@ public class Application
 	public static void main( String[] args )
 	{
 		System.out.println("Start");
+		String firstParam = null;
+		String secondParam = null;
 		try {
-			System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));
+			firstParam = args[0];
+			secondParam = args[1];
+			try {
+				System.out.println(Integer.parseInt(firstParam)/Integer.parseInt(secondParam));
+			}
+			catch(NumberFormatException exception) {
+				System.out.println("Nested try");
+			}
+			finally {
+				System.out.println("Ended outer try block");
+			}
+			
+			
 		}catch(ArrayIndexOutOfBoundsException  | NumberFormatException exception) {
 			System.out.println("Make sure you provide atleast 2 correct right argument");
 		}
