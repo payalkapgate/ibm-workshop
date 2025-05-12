97186f1af849f01d95619ed2f56be1f68aa07315
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-05-12 15:31:42 +0530

    interface

--- Typescript/js/code.js
+++ Typescript/js/code.js
@@ -1,10 +1,31 @@
-var numbers = [1, 2, 3, 4, 5];
-numbers.push(6);
-console.log(numbers);
-var fruits = ["apple", "banana", "orange"];
-fruits.push("mango");
-console.log(fruits);
-var colors = ["red", "green", "blue"];
-var mixed = ["hello", 42, "world", 100];
-mixed.push(200);
-mixed.push("typescript");
+//create object that follows the person interface
+var john = {
+    name: "Payal Kapgate",
+    age: 54,
+    greet: function () {
+        console.log("Hello, my name is ".concat(this.name, "!"));
+    }
+};
+john.greet();
+var Employee = /** @class */ (function () {
+    function Employee(name, age, jobTitle) {
+        this.name = name;
+        this.age = age;
+        this.jobTitle = jobTitle;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Hi, I'm ".concat(this.name, ", a ").concat(this.jobTitle, "."));
+    };
+    return Employee;
+}());
+var sarah = new Employee("Sarah  Smith", 28, "Developer");
+sarah.greet();
+var book1 = { title: "Ts Basics", author: "Jane Doe" };
+var book2 = { title: "Advanced TS", author: "John Smith", pages: 54 };
+var Manager = /** @class */ (function () {
+    function Manager() {
+        this.name = "BOB";
+        this.id = 234;
+    }
+    return Manager;
+}());
--- Typescript/ts/code.ts
+++ Typescript/ts/code.ts
@@ -1,13 +1,51 @@
-let numbers: number[] = [1, 2, 3, 4, 5];
-numbers.push(6);
-console.log(numbers);
+//Define an Interface for a person
+interface Person{
+    name: string;
+    age: number;
+    greet(): void; // Methos signature
+}
 
-let fruits: Array<string> = ["apple", "banana", "orange"];
-fruits.push("mango");
-console.log(fruits);
+//create object that follows the person interface
+const john: Person = {
+    name: "Payal Kapgate",
+    age: 54,
+    greet(){
+        console.log(`Hello, my name is ${this.name}!`);
+    }
+};
+john.greet();
 
-const colors: readonly string[] = ["red", "green", "blue"];
+class Employee implements Person{
+    constructor(
+        public name: string,
+        public age: number,
+        public jobTitle: string
+    ){}
+    greet(){
+        console.log(`Hi, I'm ${this.name}, a ${this.jobTitle}.`);
+    }
+}
+const sarah = new Employee("Sarah  Smith", 28, "Developer");
+sarah.greet();   
+//Interface with optional property
+interface Book{
+    title: string;
+    author: string;
+    pages?: number;
+}
 
-let mixed: (string | number)[] = ["hello", 42, "world", 100];
-mixed.push(200);
-mixed.push("typescript");
+const book1: Book = {title: "Ts Basics", author: "Jane Doe"};
+const book2: Book = {title: "Advanced TS", author: "John Smith", pages: 54}
+
+
+interface Person1{
+    name: string;
+
+}
+interface Emp extends Person1{
+    id:number;
+}
+class Manager implements Emp{
+    name = "BOB";
+    id= 234;
+}
