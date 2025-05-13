953a4f5bfaef1e87efa60c30f909b2467fd04b1e
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-05-13 16:57:39 +0530

    setter dependency injection

--- eclipse-workspace/spring/src/main/java/com/ibm/spring/TextEditor.java
+++ eclipse-workspace/spring/src/main/java/com/ibm/spring/TextEditor.java
@@ -2,8 +2,15 @@ package com.ibm.spring;
 
 public class TextEditor {
 	public SpellChecker spellChecker;
+	public TextEditor() {
+		
+	}
 	public TextEditor(SpellChecker spellChecker){
 		System.out.println("Inside Text Editor constructor");
+		this.spellChecker = spellChecker;
+	}
+	
+	public void setSpellChecker(SpellChecker spellChecker) {
 		this.spellChecker = spellChecker;
 	}
 	public void spellCheck() {
