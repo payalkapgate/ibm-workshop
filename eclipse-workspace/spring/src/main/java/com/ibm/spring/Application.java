ab896d99cdb7cb7a648337dfa4ad40218e011fd3
Payal Kapgate <Payal.Abhimanyu.Kapgate@ibm.com>
2025-05-14 10:42:11 +0530

    Annotation based configuration of bean used @Autowired

--- eclipse-workspace/spring/src/main/java/com/ibm/spring/Application.java
+++ eclipse-workspace/spring/src/main/java/com/ibm/spring/Application.java
@@ -9,11 +9,15 @@ public class Application
 	public static void main( String[] args )
 	{
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		JavaCollection javaCollection = (JavaCollection)context.getBean("javaCollection");
-		System.out.println(javaCollection.getAddressList());
-		System.out.println(javaCollection.getAddressMap());
-		System.out.println(javaCollection.getAddressSet());
-		System.out.println(javaCollection.getAddressProp()); 
-		System.out.println("End");
+		
+		TextEditor textEditor = (TextEditor)context.getBean("textEditor");
+		textEditor.spellCheck();
+		
+//		JavaCollection javaCollection = (JavaCollection)context.getBean("javaCollection");
+//		System.out.println(javaCollection.getAddressList());
+//		System.out.println(javaCollection.getAddressMap());
+//		System.out.println(javaCollection.getAddressSet());
+//		System.out.println(javaCollection.getAddressProp()); 
+//		System.out.println("End");
 	}
 }
--- eclipse-workspace/spring/src/main/java/com/ibm/spring/TextEditor.java
+++ eclipse-workspace/spring/src/main/java/com/ibm/spring/TextEditor.java
@@ -1,5 +1,7 @@
 package com.ibm.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TextEditor {
 	public SpellChecker spellChecker;
 	public TextEditor() {
@@ -9,7 +11,7 @@ public class TextEditor {
 		System.out.println("Inside Text Editor constructor");
 		this.spellChecker = spellChecker;
 	}
-	
+	@Autowired
 	public void setSpellChecker(SpellChecker spellChecker) {
 		this.spellChecker = spellChecker;
 	}
